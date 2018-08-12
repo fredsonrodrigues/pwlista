49fa80af72fb0a7149974ced98cae542425536dc
Fredson Rodrigues <fredson.rodrigues.principal@gmail.com>
2018-08-12 16:37:31 -0300

    Adicionando método para exclusão

--- README.md
+++ README.md
@@ -8,9 +8,19 @@ Pwa (Progressive Web App) é o conceito criado para definir a evolução das pá
 
 Com o advento dos aplicativos em celulares, uma pagina web com caracteristicas de Pwa consegue transmitir uma experiência mais cômoda ao usuário, sendo possível ser acessada até mesmo sem conexão com a internet - Sendo essa sua mais popular característica.
 
+Nesse projeto, aplicamos o uso de uma aplicação Pwa seguindo o checklist definido pelo Google. [Veja aqui.](https://developers.google.com/web/progressive-web-apps/checklist)
+
 ## Criando o arquivo HTML e o manifest.json:
 
-breve..
+Nesse projeto, utilizamos [Bulma](https://bulma.io/), uma poderosa ferramenta CSS baseada em Flexbox, para a estilização do html. Como trabalhamos com o [IndexdedDB](https://developer.mozilla.org/pt-BR/docs/IndexedDB), usamos a biblioteca [PouchDB](https://pouchdb.com/) para uma rápida implementação. O PouchDB tem como sua principal caracterísica a sincronização em tempo real com o CouchDB, mas não usaremos isso agora.
+
+O arquivo `manifest.json` , segundo a [Documentação do Google](https://developers.google.com/web/fundamentals/web-app-manifest/?hl=pt-br): "[...] é um arquivo JSON que permite controlar como o aplicativo web ou site é exibido para o usuário em áreas que normalmente se espera ver aplicativos".
+Então, a partir dele, conseguimos definir detalhes importantes como:
+
+* Nome da aplicação
+* Ícone da aplicação
+* Definir sua visualização (dentro ou fora do browser, ou até mesmo fullscreen)
+* Pagina inicial e splash screen
 
 ## Service Worker. O que é e como criar:
 
--- assets/js/notes.js
+++ assets/js/notes.js
@@ -6,8 +6,15 @@ var note = {
 }
 var cardlist = document.getElementById('cardList')
 var db = new PouchDB('pwlista');
+
+// limpando o formulario
+function clean() {
+    note.title = null
+    note.noteText = null
+    note._id = null
+}
 // Função que salva as notas
-function postItem() {
+function postItem(e) {
     note.title = document.getElementById('titleItem').value
     note.noteText = document.getElementById('textItem').value
     note._id = new Date().toISOString()
@@ -16,13 +23,14 @@ function postItem() {
     db.put(note, function callback(err, result) {
         if (!err) {
             closeModal()
-            alert('item criado com sucesso');
+            clean()
         }
     });
-    return false
+    e.preventDefault();
+    return false;
 }
 //Função que constroi no DOM um unico card
-function getCard(title) {
+function getCard(title, id) {
     if (title.length > 14) title = title.substring(0, 14);
     var div = document.createElement("div");
     div.className = 'card'
@@ -35,7 +43,7 @@ function getCard(title) {
                 <footer class="card-footer">
                     <p class="card-footer-item">
                         <span>
-                            <a href="#">Apagar</a>
+                            <a onclick="removeNote('${id}')">Apagar</a>
                         </span>
                     </p>
                     <p class="card-footer-item">
@@ -48,12 +56,13 @@ function getCard(title) {
 }
 // função que constrói no DOM a lista dinamicamente
 function addCard(params) {
+    cardlist.innerHTML = "";
     for (let index = 0; index < params.length; index++) {
         const el = params[index].doc;
         
         var div = document.createElement("div");
         div.className = 'column'
-        div.appendChild(getCard(el.title))
+        div.appendChild(getCard(el.title, el._id))
         cardlist.appendChild(div)
     }
 }
@@ -63,6 +72,31 @@ function listNotes() {
         addCard(doc.rows)
     });
 }
+
+// Função que irá excluir as notas salvas no banco
+function removeNote(id) {
+    this.db.get(id).then(function (doc) {
+        var c = confirm("Deseja excluir essa nota?")
+        if (c == true) {
+            alert('excluido!')
+            return db.remove(doc);
+        } else {
+            return false;
+        }
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+//Escutando as alterações
+var changes = db.changes({
+    since: 'now',
+    live: true,
+    include_docs: true
+}).on('change', function (change) {
+    console.log(change)
+    listNotes()
+})
 
 //Chamando a função assim que a pagina carregar
 window.onload = function (e) {
